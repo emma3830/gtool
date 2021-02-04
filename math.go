9f3e3d5036c712bf104a5b0c518b5dab56b0202c
emma <emmalin0822@gmail.com>
2021-02-04 17:01:29 +0800

    refactor: 調整四捨五入方法實作

--- math.go
+++ math.go
@@ -1,28 +1,22 @@
 package gtool
 
 import (
-	"math"
-
 	"github.com/shopspring/decimal"
 )
 
 // Round 四捨五入小數第n位
-// ex. f = 12.256, n = 2
-// 12.256 * (10 的 2 次方) >> 1225.6
-// 1225.6 + 0.5 >> 1226.1
-// 無條件捨去至整數 >> 1226
-// 1226 / (10 的 2 次方) >> 12.26
 func Round(f float64, n int) float64 {
-	pow := math.Pow10(n)
+	digits := int32(n)
 
-	roundInt := int(FloatAdd(FloatMul(f, pow), 0.5))
-	return FloatDiv(float64(roundInt), pow)
+	// 不需要管精準度
+	resultFloat, _ := decimal.NewFromFloat(f).Round(digits).Float64()
+	return resultFloat
 }
 
 // FloorFloatNum 指定無條件捨去到小數第Ｘ位
-func FloorFloatNum(f float64, para int) float64 {
+func FloorFloatNum(f float64, n int) float64 {
 	// 將指定的位數轉成int32
-	digits := int32(para)
+	digits := int32(n)
 	sourceNum := decimal.NewFromFloat(f)
 
 	// 位移X個位數(10進位)，取絕對值(因為可能是負數)，捨去整數後的數字，再位移回來
